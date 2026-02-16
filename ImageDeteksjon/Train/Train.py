ee302d5c81f10d1b4338de760a7d496a77000b48
Martin <you@example.com>
2026-02-16 14:52:42 +0100

    Test runs for trening av modell

--- ImageDeteksjon/Train/Train.py
+++ ImageDeteksjon/Train/Train.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 # Load a pretrained YOLO26n model
 model = YOLO("yolo26n.pt")
-results = model.predict(source="0", show= True)
 
+results = model.train(data= '/Users/martinsteinsvollrikardsen/Documents/brackish-dataset/data.yaml', epochs=1, imgsz= 640)
