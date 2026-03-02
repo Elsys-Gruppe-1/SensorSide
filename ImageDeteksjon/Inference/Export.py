ee4891513020bbefd1dc345169989d7553afa5c9
Martin <you@example.com>
2026-03-02 12:19:25 +0100

    sdjfhadshj

--- ImageDeteksjon/Inference/Export.py
+++ ImageDeteksjon/Inference/Export.py
@@ -1,4 +1,4 @@
-import ultralytics
+from ultralytics import YOLO
 model = YOLO("yolo26n.pt") ## skriv inn modell navn her
 model.export(format="ncnn") # ncnn format, spesielt bra for rasberry pi.
 
--- ImageDeteksjon/Train/Split.py
+++ ImageDeteksjon/Train/Split.py
@@ -2,6 +2,8 @@ from yolosplitter import YoloSplitter
 ys = YoloSplitter(imgFormat=['.jpg', '.jpeg', '.png'], labelFormat=['.txt'] )
 
 # If you have yolo-format dataset already on the system
-df = ys.from_yolo_dir(input_dir="yolo_dataset",ratio=(0.7,0.2,0.1),return_df=True) #input_dir, til der filene ligger
+df = ys.from_yolo_dir(input_dir="/Users/martinsteinsvollrikardsen/Documents/Elsys prosjekt/drevja data/new_dataset/full_frames",ratio=(0.7,0.2,0.1),return_df=True) #input_dir, til der filene ligger
 
 ys.info()
+
+ys.save_split(output_dir="/Users/martinsteinsvollrikardsen/Documents/Elsys prosjekt/drevja data/new_dataset")
